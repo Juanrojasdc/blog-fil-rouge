b92adde20ebeed5df7e2818f2fbf76341125861e
Juan ROJAS <juandiegorojascuicas@gmail.com>
2026-07-20 13:28:00 +0200

    Ultimo commit antes del step 3

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ function truncateString(string $string, int $length = 20): string {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php050 READ - Lecture des articles</title>
+    <title>Sport_2000</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
